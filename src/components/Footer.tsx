ae6b7477a14fe3c1e4cd634d2b5c0dc9c6dea87f
Nolan Strait <nlstrait@gmail.com>
2025-09-03 14:51:52 -0700

    working canvas

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <footer className="bg-black text-white px-4 sm:px-6 lg:px-8 p-10 align-baseline">
+            <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div>
+                    <h1 className="font-bold font-serif text-xl">JP Real Estate</h1>
+                    <h2>123-456-7890</h2>
+                    <p className="mt-3 text-sm">&copy; {new Date().getFullYear()}. Developed by Nolan Strait.</p>
+                </div>
+                <div>
+                    <h3 className="font-bold mb-3">Pages</h3>
+                    <ul>
+                        <li>
+                            <Link href="/about" className="border-b-2 border-transparent hover:border-white hover:text-gray-200">
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/stories" className="border-b-2 border-transparent hover:border-white hover:text-gray-200">
+                                Stories 
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/Contact" className="border-b-2 border-transparent hover:border-white hover:text-gray-200">
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+export default function NavBar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50); // Adjust threshold as needed; assign from variable
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <nav className={`
+        sticky w-full bg-black shadow-md transition-all duration-300 ease-in-out \
+        px-4 sm:px-6 lg:px-8 
+        ${ isScrolled ? 'h-16' : 'h-24'}
+      `}>
+      <div className="flex h-full align-middle">
+
+        <div className='flex items-center'>
+          <div className="flex-shrink-0 flex flex-col items-center align-middle">
+            <Link href="/" className="text-white font-bold font-serif text-2xl">
+              JP Real Estate
+            </Link>
+            <h2 className='text-white text-sm'>
+              123-456-7890
+            </h2>
+          </div>
+        </div>
+        <div className='grow'></div>
+
+        <div className="hidden font-sans text-base text-white font-medium sm:ml-6 sm:flex sm:space-x-8">
+          <Link
+            href="/about"
+            className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent hover:border-white hover:text-gray-200"
+          >
+            About
+          </Link>
+          <Link
+            href="/stories"
+            className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent hover:border-white hover:text-gray-200"
+          >
+            Stories
+          </Link>
+          <Link
+            href="/contact"
+            className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent hover:border-white hover:text-gray-200"
+          >
+            Contact
+          </Link>
+        </div>
+
+      </div>
+    </nav>
+  );
+};
